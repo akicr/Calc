6565a076bec424f45253179eb8e7bd9f910f1700
user <malyhandrej888@gmail.com>
2026-09-15 22:33:57 +0500

    готовый кроме умножения 12

--- calculator.java
+++ calculator.java
@@ -5,11 +5,11 @@ public class calculator {
         Scanner scanner = new Scanner(System.in);
         System.out.println("=== Калькулятор ===");
         System.out.print("Введите первое число: ");
-        double a = scanner.nextInt();
+        double a = scanner.nextDouble();
         System.out.print("Введите операцию (+, -, *, /): ");
         String op = scanner.next();
         System.out.print("Введите второе число: ");
-        double b = scanner.nextInt();
+        double b = scanner.nextDouble();
         double result;
         if (op.equals("+")) {
             result = add.add1(a, b);
